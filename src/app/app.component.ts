478718893ef869796e8d093a6f51386cb8e99071
Esperanza Cutillas Rastoll <especr101@gmail.com>
2020-02-10 18:08:56 +0100

    We show some static data on the page.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Book} from './common/book';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,43 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'fiction-world-store';
+
+  books: Book[] = [
+    {
+      title: "C Programming Language",
+      author: "",
+      isbn: "",
+      description: "Learn C Programming Language",
+      unitPrice: 60,
+      imageUrl: "assets/images/books/text-100.png",
+      active: true,
+      unitsStock: 100,
+      dateCreated: new Date(),
+      dateUpdated: null,
+    },
+    {
+      title: "C# Crash Course",
+      author: "",
+      isbn: "",
+      description: "Learn C# Programming Language",
+      unitPrice: 90,
+      imageUrl: "assets/images/books/text-101.png",
+      active: true,
+      unitsStock: 100,
+      dateCreated: new Date(),
+      dateUpdated: null,
+    },
+    {
+      title: "C++ Crash Course",
+      author: "",
+      isbn: "",
+      description: "Learn C++ Programming Language",
+      unitPrice: 70,
+      imageUrl: "assets/images/books/text-102.png",
+      active: true,
+      unitsStock: 100,
+      dateCreated: new Date(),
+      dateUpdated: null,
+    }
+  ]
 }
