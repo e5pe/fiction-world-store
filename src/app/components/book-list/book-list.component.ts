e0d6bf855414c92e5029887e45d1bc1b4500421b
Esperanza Cutillas Rastoll <especr101@gmail.com>
2020-02-25 00:53:50 +0100

    Footer changed and new template for the books created.

--- src/app/components/book-list/book-list.component.ts
+++ src/app/components/book-list/book-list.component.ts
@@ -1,27 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Book } from '../../common/book';
 import { BookService } from 'src/app/services/book.service';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-book-list',
-  templateUrl: './book-list.component.html',
+  // templateUrl: './book-list.component.html',
+  templateUrl: './book-grid.component.html',
   styleUrls: ['./book-list.component.css']
 })
 export class BookListComponent implements OnInit {
   books: Book[];
-  constructor(private bookService: BookService) { }
+  @Input() templateSelected = 'grid';
+  constructor(private bookService: BookService) {}
 
   ngOnInit() {
+    // this.selectView();
     this.listBooks();
   }
 
-  listBooks(){
-    this.bookService.getBooks().subscribe(
-      data => {
-        console.log(data);
-        this.books = data;
-      }
-    );
+  listBooks() {
+    this.bookService.getBooks().subscribe(data => {
+      console.log(data);
+      this.books = data;
+    });
   }
 
+  selectView() {
+    let name = 'grid';
+    switch (this.templateSelected) {
+      case 'grid':
+      case 'list':
+        break; // It remains the same
+      default:
+        this.templateSelected = 'grid';
+        break;
+    }
+    let nameUrl = `./book-${name}.component.html`;
+
+  }
 }
