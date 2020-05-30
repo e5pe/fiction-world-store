2f156d4484616b7c503186b46d8c3268b957e8dc
Esperanza Cutillas Rastoll <especr101@gmail.com>
2020-05-30 18:16:13 +0200

    Currency displays now on the right side.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { BookListComponent } from './components/book-list/book-list.component';
@@ -8,6 +8,17 @@ import { SearchBoxComponent } from './components/search-box/search-box.component
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import { RouterModule, Routes } from '@angular/router';
 import { BookCategoryComponent } from './components/book-category/book-category.component';
+
+
+
+// import locals
+import localeES from '@angular/common/locales/es';
+import localeEn from '@angular/common/locales/en';
+import { registerLocaleData } from '@angular/common';
+
+// registrar los locales con el nombre que quieras utilizar a la hora de proveer registerLocaleData(localePy, 'es'); registerLocaleData(localePt, 'pt'); registerLocaleData(localeEn, 'en') registerLocaleData(localeEsAR, 'es-Ar');
+registerLocaleData(localeEn, 'en');
+registerLocaleData(localeES, 'es');
 
 const routes: Routes = [
   { path: 'books', component: BookListComponent },
@@ -23,4 +34,4 @@ const routes: Routes = [
   providers: [BookService],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
