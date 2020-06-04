c97648eb0c16e0550338002a06c474329cfdd4c8
Esperanza Cutillas Rastoll <especr101@gmail.com>
2020-06-04 18:00:57 +0200

    Back button of deails book view is working.

--- src/app/components/book-details/book-details.component.ts
+++ src/app/components/book-details/book-details.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-book-details',
   templateUrl: './book-details.component.html',
-  styleUrls: ['./book-details.component.css']
+  styleUrls: ['./book-details.component.css'],
 })
 export class BookDetailsComponent implements OnInit {
+  constructor(private location: Location) {}
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit() {}
+  goBack() {
+    this.location.back();
   }
-
 }
